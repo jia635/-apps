ce5f54403c518e4aaa27be48bf50367df594a48a
lang <3004520294@qq.com>
2017-11-16 18:10:41 +0800

    基础 Project

---
+++ Apps/app/src/main/java/com/langwang/apps/MainActivity.java
@@ -0,0 +1,12 @@
+package com.langwang.apps;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
